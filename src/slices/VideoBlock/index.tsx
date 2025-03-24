e0beb228f1e6c1892d4304ff461952b69301c674
draudrichman <88120992+draudrichman@users.noreply.github.com>
2025-03-24 15:50:53 +0600

    added video block

--- src/slices/VideoBlock/index.tsx
+++ src/slices/VideoBlock/index.tsx
@@ -1,24 +1,61 @@
 import { FC } from "react";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
+import { Bounded } from "@/components/Bounded";
+import { LazyYouTubePlayer } from "./LazyYouTubePlayer";
+import clsx from "clsx";
+import { isFilled } from "@prismicio/client";
+import Image from "next/image";
 
-/**
- * Props for `VideoBlock`.
- */
+const MASK_CLASSES = "[mask-image:url(/video-mask.png)] [mask-mode:alpha] [mask-position:center_center] [mask-repeat:no-repeat] [mask-size:100%_auto]";
+
 export type VideoBlockProps = SliceComponentProps<Content.VideoBlockSlice>;
 
-/**
- * Component for "VideoBlock" Slices.
- */
+
 const VideoBlock: FC<VideoBlockProps> = ({ slice }) => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      className="bg-texture bg-zinc-900"
     >
-      Placeholder component for video_block (variation: {slice.variation})
-      Slices
-    </section>
+      <h2 className="sr-only">Video Reel</h2>
+      <div className="relative aspect-video">
+        {/* Masks */}
+        <div
+          className={clsx(
+            MASK_CLASSES,
+            "bg-brand-lime absolute inset-0 ~translate-x-2/3 ~translate-y-2/3"
+          )}
+        />
+        <div
+          className={clsx(
+            MASK_CLASSES,
+            "bg-white absolute inset-0 ~translate-x-1/3 ~translate-y-1/2"
+          )}
+        />
+        <div
+          className={clsx(
+            MASK_CLASSES,
+            "bg-white absolute inset-0 ~translate-x-1/2 ~-translate-y-1/3"
+          )}
+        />
+        {/* Video */}
+        <div className={clsx(MASK_CLASSES, "relative h-full")}>
+          {isFilled.keyText(slice.primary.youtube_video_id) ? (
+            <LazyYouTubePlayer youTubeID={slice.primary.youtube_video_id} />
+          ) : null}
+          {/* Texture overlay */}
+          <Image
+            src="/image-texture.png"
+            alt=""
+            fill
+            className="pointer-events-none object-cover opacity-50"
+          />
+        </div>
+
+      </div>
+    </Bounded>
   );
 };
 
