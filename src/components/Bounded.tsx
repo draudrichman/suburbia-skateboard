20413d44041eb6adfdf2ed1af99d66407beab296
draudrichman <88120992+draudrichman@users.noreply.github.com>
2025-03-29 07:58:39 +0600

    fixed again

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,28 +1,28 @@
-import { CSSProperties, ElementType, ReactNode } from "react";
+import { CSSProperties, ReactNode } from "react";
 import clsx from "clsx";
 
 type BoundedProps = {
-    // as?: ElementType;
+    as?: "section" | "nav";
     className?: string;
     style?: CSSProperties;
     children: ReactNode;
 };
 
 export function Bounded({
-    // as: Comp = "section",
+    as: Comp = "section",
     className,
     children,
     ...restProps
 }: BoundedProps) {
     return (
-        <section
+        <Comp
             className={clsx(
                 "px-6 ~py-10/16 [.header+&]:pt-44 [.header+&]:md:pt-32",
-                className,
+                className
             )}
             {...restProps}
         >
             <div className="mx-auto w-full max-w-6xl">{children}</div>
-        </section>
+        </Comp>
     );
 }
